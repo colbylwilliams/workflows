f22ef750c1ddceff0653eeb147478bf78cd41a73
Colby Williams <ColbyLWilliams@gmail.com>
2022-05-12 16:53:11 -0500

    parse yaml

--- .github/actions/msdev/src/main.ts
+++ .github/actions/msdev/src/main.ts
@@ -1,5 +1,17 @@
 import * as core from '@actions/core';
 import * as glob from '@actions/glob';
+import * as fs from 'fs/promises';
+import * as yaml from 'js-yaml';
+
+interface AzureYml {
+    fidalgo: AzureYmlFidalgo;
+
+}
+
+interface AzureYmlFidalgo {
+    project: string;
+    catalog_item: string;
+}
 
 async function run(): Promise<void> {
     try {
@@ -9,6 +21,29 @@ async function run(): Promise<void> {
 
         const globber = await glob.create(azureyml);
         const files = await globber.glob();
+
+        const file = files.length > 0 ? files[0] : undefined;
+
+        if (file) {
+            core.info(`Found azure.yml file: ${file}`);
+
+            const contents = await fs.readFile(file, 'utf8');
+
+            const data = yaml.load(contents) as AzureYml;
+
+            const project = data.fidalgo.project;
+
+            if (project) {
+                core.info(`Found project in azure.yml file: ${project}`);
+                core.setOutput('project', project);
+            } else {
+                core.setFailed(`Could not get project from azure.yml: ${contents}`);
+            }
+
+
+        } else {
+            core.setFailed(`Could not find azure.yml file with specified glob: ${azureyml}`);
+        }
 
         files.forEach(file => {
             core.info(`Found file: ${file}`);
